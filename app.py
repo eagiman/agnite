b89c24e5f3a1163ef0e2cee1399c85574d64f189
EAGIMAN <anniegiman@gmail.com>
2023-05-05 17:10:13 -0400

    updated comments and removed old unused code

--- app.py
+++ app.py
@@ -10,11 +10,7 @@ import streamlit_toggle as tog
 from streamlit_extras.stateful_button import button as Button
 from streamlit_extras.add_vertical_space import add_vertical_space
 
-# if 'sidebar_state' not in st.session_state:
-#     st.session_state.sidebar_state = 'expanded'
-
 # Configure page
-#st.set_page_config(page_title="AGNITE", page_icon='assets/agnite.png', layout='wide', initial_sidebar_state=st.session_state.sidebar_state)
 st.set_page_config(page_title="AGNITE", page_icon='assets/agnite.png', layout='wide')
 
 # Initialize angle to 0
@@ -22,23 +18,33 @@ if 'angle' not in st.session_state:
     st.session_state['angle'] = 0
     st.session_state.angle = 0
 
+# Initialize display lines to True
 if 'lines' not in st.session_state:
     st.session_state.lines = True
     st.session_state['lines'] = True
 
+# Initialize default to narrow line galaxy
 if 'default' not in st.session_state:
     st.session_state.default = 'Narrow Line Radio Galaxy'
     st.session_state['default'] = 'Narrow Line Radio Galaxy'
 
+# Initialize about to False
 if 'about' not in st.session_state:
     st.session_state['about'] = False
 
+# Make columns for title and about button
 col1, col2 = st.columns([8,1])
 
+# Add about button
+# Using Streamlit Extra's stateful button to track whether to display info or not
 with col2:
     add_vertical_space(1)
     Button("About", key='about')
 
+# give title
+col1.header('Active Galactic Nuclei Interactive Tool for Exploration')
+
+# Give about section
 if st.session_state['about']:
     st.info("""
     This is a tool to explore the 
@@ -49,36 +55,18 @@ if st.session_state['about']:
     for different viewing angles and types.
     """)
 
-# if st.session_state['about']:
-#     st.session_state.sidebar_state = 'collapsed'
-#     st.markdown("""---""")
-#     info1, info2 = st.columns(2)
-#     info2.image('assets/agn_tight.png')
-#     st.markdown("""---""")
-# else:
-#     st.session_state.sidebar_state = 'expanded'
-
-# give title
-col1.header('Active Galactic Nuclei Interactive Tool for Exploration')
-#st.write('It is going to say more here!')
-
-# # Makes about section
-# with st.expander('What are AGN?'):
-#     col1, col2 = st.columns(2)
-#     col2.image('assets/agn_tight.png')
-#     col1.write("Hello!")
-
-
+# Create dictionary matching AGN type with default angle for use with selectbox
 type = {
     'Blazar': 80,
     'Radio-Loud Quasar': 60,
     'Radio-Quiet Quasar': -80,
     'Broad Line Radio Galaxy': 30,
-    'Narrow Line Radio Galaxy': 10,
+    'Narrow Line Radio Galaxy': 0,
     'Seyfert 1': -60,
     'Seyfert 2': -30
 }
 
+# Create dictionary matching AGN type with index to use for selectbox default
 ind = {
     'Blazar': 0,
     'Radio-Loud Quasar': 1,
@@ -89,12 +77,22 @@ ind = {
     'Seyfert 2': 6
 }
 
+
+# Create model and agn objects to initialize data and visualization
 mod = model.Model(r=910)
 agn = spectra.AGN(st.session_state.angle)
 
 
+def make_spec(lines):
+    """
+    Function for creating plotly figure displaying spectrum
 
-def make_spec(angle, lines):
+    Args:
+        lines (bool): Determine whether to display emission line markers
+
+    Returns:
+        fig (plotly figure): figure of spectrum with or without emission lines marked
+    """
     df = agn.df
     fig = px.line(df, x='Wavelength', y='Flux', labels={
         "Wavelength": "Wavelength (&#197;)", "Flux": "Flux (erg cm<sup>-2</sup> s<sup>-1</sup> &#197;<sup>-1</sup>)"})
@@ -103,7 +101,13 @@ def make_spec(angle, lines):
     return fig
 
 
-def make_sed(angle):
+def make_sed():
+    """
+    Function for creating plotly figure displaying SED
+
+    Returns:
+        fig (plotly figure): figure of SED
+    """
     df = agn.get_sed()
     fig = px.scatter(df, x='Frequency', y='Density', labels={
         'Frequency': 'Frequency (Hz)', 'Density': 'Flux Density (Jy)'}, log_x=True, log_y=True)
@@ -111,11 +115,19 @@ def make_sed(angle):
 
 
 @st.cache_data
-def run(angle, lines, path=None):
+def run(lines):
+    """
+    Cached driver function to update plots as user input changes
 
+    Args:
+        lines (bool): Determine whether to display emission line markers
+
+    Returns:
+        Dictionary of spectrum figure, SED figure, AGN type string, and AGN object string
+    """
     agn.rotate(st.session_state.angle)
-    spec = make_spec(angle, lines)
-    sed = make_sed(angle)
+    spec = make_spec(lines)
+    sed = make_sed()
 
     vals = {'spec': spec, 'sed': sed, 'type': agn.type, 'obj': agn.obj}
 
@@ -124,79 +136,48 @@ def run(angle, lines, path=None):
 
 @st.cache_data
 def make_model(angle):
+    """
+    Cached function to update arrow orientation on visualization model
+
+    Args:
+        angle (int): Viewing angle in degrees
+
+    Returns:
+        (PIL Image): image with arrow pasted with updated location and angle
+    """
     return mod.paste(angle)
 
-# st.sidebar.image('/Users/annie/Desktop/Astr330/agnite/agn.png',
-#                  use_column_width=True)
-# st.sidebar.image(get_model(st.session_state.channel),
-#                  use_column_width=True)
 
-
+# Add slider in sidebar for user to input angle
 st.session_state.angle = st.sidebar.slider('Galaxy Viewing Angle',
                             min_value=-90, max_value=90, value=type[st.session_state['default']], format="%+d°\n")
 
+# Display model in sidebar using cached function, so it updates with user input
 st.sidebar.image(make_model(st.session_state.angle),
                  use_column_width='always')
-# st.sidebar.image(run(st.session_state.angle, st.session_state['lines'])['model'],
-#                  use_column_width=True)
 
-# if st.sidebar.button(label='Radio-Quiet Quasar', use_container_width=True):
-#     st.session_state.angle = -80
-#     st.session_state.default = -80
-#
-# if st.sidebar.button(label='Seyfert 1', use_container_width=True):
-#     st.session_state.angle = -60
-#     st.session_state.default = -80
-#
-# if st.sidebar.button(label='Seyfert 2', use_container_width=True):
-#     st.session_state.angle = -30
-#     st.session_state.default = -80
-#
-# if st.sidebar.button(label='Narrow Line Radio Galaxy', use_container_width=True):
-#     st.session_state.angle = 10
-#     st.session_state.default = -80
-#
-# if st.sidebar.button(label='Broad Line Radio Galaxy', use_container_width=True):
-#     st.session_state.angle = 30
-#
-#
-# if st.sidebar.button(label='Radio-Loud Quasar', use_container_width=True):
-#     st.session_state.angle = 60
-#
-# if st.sidebar.button(label='Blazar', use_container_width=True):
-#     st.session_state.angle = 80
-
-
-
-# if st.sidebar.button(label='Blazar'):
-#     st.session_state.angle = 80
-#     default = 80
-
-# if st.sidebar.button(label='Blazar'):
-#     st.session_state.angle = 80
-#     default = 80
-#
-# if st.sidebar.button(label='Blazar'):
-#     st.session_state.angle = 80
-#     default = 80
-
+# Add selectbox for user to view based on AGN type rather than angle, and set angle to default
 st.sidebar.selectbox('AGN Type',
     ('Blazar', 'Radio-Loud Quasar', 'Radio-Quiet Quasar', 'Broad Line Radio Galaxy',
      'Narrow Line Radio Galaxy', 'Seyfert 1', 'Seyfert 2'), key='default', index=ind[st.session_state['default']])
 
-
+# Set up columns for metrics
 metric1, metric2 = st.columns(2)
 
-metric1.metric(label='AGN Type', value=run(st.session_state.angle, st.session_state['lines'])['type'])
-metric2.metric(label='Object', value=run(st.session_state.angle, st.session_state['lines'])['obj'])
+# Create metrics displaying current AGN type and Obejct name
+metric1.metric(label='AGN Type', value=run(st.session_state['lines'])['type'])
+metric2.metric(label='Object', value=run(st.session_state['lines'])['obj'])
 
+# Create tabs for viewing spectrum and SED
 tab_spec, tab_sed = st.tabs(["Spectrum", "SED"])
 
+# Display spectrum and toggle button for displaying emission lines in spectrum tab
 with tab_spec:
 
     tog.st_toggle_switch(label="Display Emission Lines", key='lines', default_value=True)
-    st.plotly_chart(run(st.session_state.angle, st.session_state['lines'])['spec'], use_container_width=True)
+    st.plotly_chart(run(st.session_state['lines'])['spec'], use_container_width=True)
 
+# Display SED in sed tab
 with tab_sed:
 
-    st.plotly_chart(run(st.session_state.angle, st.session_state['lines'])['sed'], use_container_width=True)
+    st.plotly_chart(run(st.session_state['lines'])['sed'], use_container_width=True)
--- spectra.py
+++ spectra.py
@@ -3,6 +3,7 @@ from astroquery.ipac.ned import Ned
 import numpy as np
 import pandas as pd
 
+
 def no_zero(wave, flux):
     """
     Function that gets rid of 0 values in wavelength and flux arrays
@@ -36,51 +37,6 @@ def open_spec(path, file):
     fluxfull = data['Flux density']
     wave, flux = no_zero(wavefull, fluxfull)
     return wave, flux
-
-
-def get_spec(angle):
-    """
-    Function to get AGN classification based on approximate viewing angle.
-    Uses rough definitions of Emma Alexander and data from BASS Data Release 1.
-
-    Args:
-        angle (int): Approximate AGN viewing angle
-
-    Returns:
-        wave (np array): rest wavelength
-        flux (np array): flux density
-    """
-    angle += 90
-    if (angle >= 0) & (angle < 15):
-        type = 'blazar'
-        num = '0545'
-    elif (angle >= 15) & (angle < 45):
-        type = 'radio-loud quasar'
-        num = '0715'
-    elif (angle >= 45) & (angle < 70):
-        type = 'broad line radio galaxy'
-        num = '1110'
-    elif (angle >= 70) & (angle < 90):
-        type = 'narrow line radio galaxy'
-        # Maybe also 474? But that's a Seyfert 1 which doesn't make sense?
-        # Also 360
-        num = '0474'
-    # TODO: add Sy 1.2, 1.5, 1.7, 1.8, 1.9
-    elif (angle >= 90) & (angle < 135):
-        type = 'sy2'
-        # Also 10, 49,
-        num = '0007'
-    elif (angle >= 135) & (angle < 160):
-        type = 'sy1'
-        num = '0002'
-    elif (angle >= 160) & (angle <= 180):
-        type = 'radio-quiet quasar'
-        num = '1146'
-
-    path = 'assets/BASS_DR1_fits.zip/'
-    file = 'BASS_DR1_' + num + '.fits'
-
-    return open_spec(path, file)
 
 
 class AGN:
@@ -124,7 +80,6 @@ class AGN:
             # Also 360
             self.num = '0474'
             self.obj = 'VII Zw 292'
-        # TODO: add Sy 1.2, 1.5, 1.7, 1.8, 1.9
         elif (angle >= -45) & (angle < 0):
             self.type = 'Seyfert 2'
             # Also 10, 49,
@@ -177,7 +132,6 @@ class AGN:
             # Also 360
             self.num = '0474'
             self.obj = 'VII Zw 292'
-            # TODO: add Sy 1.2, 1.5, 1.7, 1.8, 1.9
         elif (angle >= -45) & (angle < 0):
             self.type = 'Seyfert 2'
             # Also 10, 49,
@@ -209,8 +163,8 @@ class AGN:
         Method to get rest wavelength and flux density out of FITS file
 
         Returns:
-            wave: rest wavelength
-            flux: flux density
+            wave (ndarray): rest wavelength
+            flux (ndarray): flux density
         """
         return self.wave, self.flux
 
@@ -283,14 +237,14 @@ class AGN:
                 "O3b": (5006.843, r"[OIIIb]"),
                 "N1": (5200.257, r"[NI]"),
                 "S2": (6718.32, r"[SII]"),
-                "O1": (6363.776, r"[OI]"),
+                "O1_1": (6363.776, r"[OI]"),
                 "Fe7": (6087.000, r"[FeVII]"),
                 "H-gamma": (4340.471, r"Hγ"),
                 "Cl3a": (5517.709, r"ClIIIa"),
                 "Cl3b": (5537.873, r"ClIIIb"),
                 "Ar3": (7135.790, r"[ArIII]"),
                 "O2": (7330.730, r"[OII]"),
-                "O1": (6300.304, r"[OI]"),
+                "O1_2": (6300.304, r"[OI]"),
                 "H-alpha": (6562.819, r"Hα"),
                 "N2": (6585.23, r"[NII]")
                 # "Fe10": (6374.510, r"[FeX")

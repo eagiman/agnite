bc89351279ed3f3b54f94e571597bad35e314dbf
EAGIMAN <anniegiman@gmail.com>
2023-05-05 17:27:21 -0400

    adding installability function

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='agnite',
+    version='0.1.0',
+    description='Active Galactic Nuclei Interactive Tool for Exploration',
+    url='https://github.com/eagiman/agnite',
+    author='Annie Giman',
+    author_email='annie.giman@yale.edu',
+    license='MIT',
+    packages=['agnite'])

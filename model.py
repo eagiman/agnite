a689d4f7e3143206b310855ac724477d834dd751
EAGIMAN <anniegiman@gmail.com>
2023-05-05 03:32:19 -0400

    Added Audrey AGN

--- model.py
+++ model.py
@@ -20,23 +20,29 @@ class Model:
         y (int): y coordinate in pixels for arrow to be pasted on image
     """
     def __init__(self, angle=0, r=1220):
-        self.im = Image.open('assets/agn.png')
+        self.im = Image.open('assets/agn_new.png')
         self.arrow = Image.open('assets/arrow.webp').rotate(180, expand=True)
+        self.arrow = self.arrow.resize((int(.7 * self.arrow.size[0]), int(.7 * self.arrow.size[1])))
         self.angle = angle
         self.arrow = self.arrow.rotate(self.angle, expand=True)
         self.r = r
+        self.center_x = self.im.size[0] // 4 + 57
+        self.center_y = self.im.size[1] // 2 - 100
 
         self.rad = np.radians(self.angle)
-        self.x = int(r * np.cos(self.rad))
-        self.y = int(r * np.sin(self.rad))
+        # self.x = int(r * np.cos(self.rad))
+        # self.y = int(r * np.sin(self.rad))
+        self.x = self.center_x + int(r * np.cos(self.rad))
+        self.y = self.center_y - int(r * np.sin(self.rad))
 
     def refresh(self):
         """
         Function that resets the im and arrow attributes to default after
         changing through pasting or rotation
         """
-        self.im = Image.open('assets/agn.png')
+        self.im = Image.open('assets/agn_new.png')
         self.arrow = Image.open('assets/arrow.webp').rotate(180, expand=True)
+        self.arrow = self.arrow.resize((int(.7 * self.arrow.size[0]), int(.7 * self.arrow.size[1])))
 
     def rotate(self, angle):
         """
@@ -53,9 +59,10 @@ class Model:
         self.angle = angle
         self.arrow = self.arrow.rotate(angle, expand=True)
         self.rad = np.radians(self.angle)
-        self.x = self.im.size[0]//2 - 60 + int(r * np.cos(self.rad))
-        self.y = self.im.size[1]//2 - int(r * np.sin(self.rad))
-        pos = (self.x, self.y)
+        x = self.center_x + int(r * np.cos(self.rad))
+        y = self.center_y - int(r * np.sin(self.rad))
+        self.x, self.y = x, y
+        pos = (x, y)
         return pos
 
     def turn(self, angle):
